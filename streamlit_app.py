ae19a8a7b244b236dacc4863889baca5d4625360
sontush123 <tprasantpatro0@gmail.com>
2023-07-13 12:25:43 +0530

    remove movie poster

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,46 @@
 import pickle
 import streamlit as st
 import zipfile
-st.title("Content based Movie Recommender System")
+#st.title("Content based Movie Recommender System")
 #st.write('Hello world!')
+# Adjust the size of the first header using CSS styling
+header1_style = "<style>h1.header1{font-size: 32px;}</style>"
+st.markdown(header1_style, unsafe_allow_html=True)
+
+# Display the first header with the adjusted size
+st.markdown("<h1 class='header1'>Movie Recommender System</h1>", unsafe_allow_html=True)
+
+
+
+#st.header('Movie Recommender System')
+
+import webbrowser
+
+def open_github_repo():
+    url = 'https://github.com/sontush123/content_based_movie_recommender_system.git'
+    webbrowser.open_new_tab(url)
+
+
+col1, col2 = st.columns([3, 1])  # Adjust the column widths as needed
+# Adjust the size of the second header using CSS styling
+header2_style = "<style>h1.header2{font-size: 24px;}</style>"
+st.markdown(header2_style, unsafe_allow_html=True)
+
+# Display the second header with the adjusted size
+#st.markdown("<h1 class='header2'>Header 2</h1>", unsafe_allow_html=True)
+# Add the header to the first column
+#col1.header('Welcome to My App')
+col1.markdown("<h1 class='header2'>Welcome to My App</h1>", unsafe_allow_html=True)
+# Add the button to the second column
+if col2.button('CODE OF THIS PROJECT'):
+    open_github_repo()
+
+
+
+
+
+
+
 #movie_dict = pickle.load(open(r"C:\Users\prasant\Desktop\DATASCIENCE\Projects\content_based_movie_recommender_system\movie-recommender-system-tmdb-dataset-main\movie_list.pkl",'rb'))
 #similarity_matrix = pickle.load(open(r"C:\Users\prasant\Desktop\DATASCIENCE\Projects\content_based_movie_recommender_system\movie-recommender-system-tmdb-dataset-main\similarity.pkl","rb"))
 movies = pickle.load(open("movie_list.pkl",'rb'))
@@ -34,7 +72,7 @@ def similarity_fun(movie):
 
 
 movie_list = movies['title'].values
-selected_movie_name = st.selectbox('How would you like to be contracted ?',movie_list)
+selected_movie_name = st.selectbox('Type or select a movie from the dropdown',movie_list)
 if st.button("Click To Get Top 5 Similarity"):
     recomendation = similarity_fun(selected_movie_name)
     for i in recomendation:
